54be5f9235332c06a85524a905f089ca0d9d2170
Shreedevi <152841672+Shreedevi17-bc@users.noreply.github.com>
2025-08-07 23:37:42 +0530

    Create squareofn.py

---
+++ squareofn.py
@@ -0,0 +1,7 @@
+# Input: Size of the square
+N = int(input("Enter a number: "))
+
+# Loop through rows
+for i in range(N):
+    # Print the same number 'i' N times in each row
+    print(str(i) * N)
